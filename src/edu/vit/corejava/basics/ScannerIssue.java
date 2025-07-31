3ab143ccf09448f41935902af204b51c0f13afeb
anthoniraj <anthoniraja@gmail.com>
2025-07-31 14:07:33 +0530

    commented unused code

--- src/edu/vit/corejava/basics/ScannerIssue.java
+++ src/edu/vit/corejava/basics/ScannerIssue.java
@@ -6,9 +6,9 @@ import java.util.Scanner;
 public class ScannerIssue {
    public static void main(String[] args) {
     int a = Math.abs(20);
-    double squareRoot = Math.sqrt(3);
-    int maxValue = Math.max(1, 2);
-    int minValue = Math.min(10,2);
+   //  double squareRoot = Math.sqrt(3);
+   //  int maxValue = Math.max(1, 2);
+   //  int minValue = Math.min(10,2);
     
 
     Scanner sc = new Scanner(System.in);
@@ -20,6 +20,7 @@ public class ScannerIssue {
     System.out.println("Enter Name: ");
     String name = sc.nextLine();
     System.out.println("Welcome "+ name);
+    sc.close();
    }
 
 }
